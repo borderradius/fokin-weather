d2ef7ecbd38370d15e6ff687d808847ff2732cbe
spacemonkey <spacemonkey925@gmail.com>
2020-12-16 15:38:32 +0900

    날씨api 통신

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Alert } from 'react-native';
 import Loading from './Loading';
+import Weather from './Weather';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+import axios from 'axios';
 
-export default function App() {
-  return <Loading></Loading>
-    
+const API_KEY = "2c1742f7f319ae13720039a0a04200ef"
+
+export default class extends React.Component {
+  state = {
+    isLoading: true
+  }
+  getWeather = async (lat, lon) => {
+    try {
+
+      const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+      console.warn('날짜데이터', data);
+      this.setState({isLoading: false, temp: data.main.temp})
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  getLocation = async () => {
+    try {
+      const {status} = await Permissions.askAsync(Permissions.LOCATION)
+
+      if(status === 'granted'){
+        const location = Location.getCurrentPositionAsync({enableHighAccuracy: true})
+        location.then(({coords: {latitude, longitude}}) => {
+          console.log(latitude, longitude)
+          this.getWeather(latitude, longitude)
+        })
+      }
+    } catch (e) {
+      console.error(e);  
+    }
+  }
   
+  componentDidMount() {
+    this.getLocation()
+  }
+  render (){
+    const {isLoading, temp} = this.state
+    return  isLoading? <Loading /> : <Weather temp={Math.round(temp)} />
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    // flexDirection: 'row',
-    // backgroundColor: '#fff',
-    // alignItems: 'center',
-    // justifyContent: 'center',
   },
   yellowView: {
     flex: 1,
