8c93bfd4a6f190d873c26a4c69f3e737a20f60f5
spacemonkey <spacemonkey925@gmail.com>
2020-12-16 17:55:17 +0900

    api결과에 따른 날씨화면 구현

--- App.js
+++ App.js
@@ -16,9 +16,13 @@ export default class extends React.Component {
   getWeather = async (lat, lon) => {
     try {
 
-      const {data} = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
-      console.warn('날짜데이터', data);
-      this.setState({isLoading: false, temp: data.main.temp})
+      const {
+        data:{
+          main:{temp},
+          weather
+        }
+      } = await axios.get(`http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+      this.setState({isLoading: false, temp, condition: weather[0].main})
     } catch (e) {
       console.log(e);
     }
@@ -43,8 +47,8 @@ export default class extends React.Component {
     this.getLocation()
   }
   render (){
-    const {isLoading, temp} = this.state
-    return  isLoading? <Loading /> : <Weather temp={Math.round(temp)} />
+    const {isLoading, temp, condition} = this.state
+    return  isLoading? <Loading /> : <Weather temp={Math.round(temp)} condition={condition} />
   }
 }
 
--- Weather.js
+++ Weather.js
@@ -1,14 +1,36 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, StatusBar} from 'react-native';
 import PropTypes from 'prop-types';
+import {MaterialCommunityIcons} from '@expo/vector-icons';
+import {LinearGradient} from 'expo-linear-gradient';
 
-export default function Weather({temp}) {
-  return <View style={styles.container}><Text>{temp}</Text></View>
+const weatherOptions = {
+  Clear: {
+    iconName: 'weather-partly-cloudy',
+    gradient: ['#d39d38','#4da0b0']
+  }
+}
 
+export default function Weather({temp, condition}) {
+  return  <LinearGradient
+  // Background Linear Gradient
+  colors={weatherOptions[condition].gradient}
+  style={styles.container}
+>
+<StatusBar barStyle="light-content" />
+<View style={styles.halfContainer}>
+<MaterialCommunityIcons size={96} name={weatherOptions[condition].iconName || 'weather-sunset'} style={styles.text} />
+<Text style={styles.temp}>{temp}°</Text>  
+</View>
+<View style={styles.halfContainer}>
+  <Text style={styles.text}>lorem</Text>
+</View>
+</LinearGradient>   
 }
 
 Weather.propTypes = {
-  temp: PropTypes.number.isRequired
+  temp: PropTypes.number.isRequired,
+  condition: PropTypes.oneOf(['Thunderstorm','Drizzle','Rain','Snow','Clear','Clouds','Atmosphere']).isRequired
 }
 
 const styles = StyleSheet.create({
@@ -16,6 +38,20 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
+  },
+  temp: {
+    fontSize: 42,
+    fontWeight: 'bold',
+    color: 'white'
+  },  
+  text: {
+    color: 'white'
+  },
+  halfContainer: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    
   }
 })
 
